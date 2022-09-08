0419422367f6c2b0e55741d698cc2173a53cc542
Maksym <lucky13375@gmail.com>
2022-09-08 21:36:32 +0200

    added files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup)
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(picture => {
+        return `<div class='gallery__item'>
+        <img src='${picture.preview}' class='gallery__image' alt='Picture'></div>`
+    }).join('')
+}
+
+createGalleryMarkup(galleryItems);
 
 console.log(galleryItems);
