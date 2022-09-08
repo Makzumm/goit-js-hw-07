ddb7ff57de23f37cb5c87aeb493217c6378a0c4a
Maksym <lucky13375@gmail.com>
2022-09-08 22:54:58 +0200

    finished 1st gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,12 +7,49 @@ const galleryMarkup = createGalleryMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup)
 
 function createGalleryMarkup(galleryItems) {
-    return galleryItems.map(picture => {
-        return `<div class='gallery__item'>
-        <img src='${picture.preview}' class='gallery__image' alt='Picture'></div>`
+    return galleryItems.map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
     }).join('')
 }
 
 createGalleryMarkup(galleryItems);
+
+galleryContainer.addEventListener('click',onGalleryContainerClick)
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault()
+
+    const event = evt.target;
+    const click = event.classList.contains
+    ('gallery__image');
+    const chosenImg = event.dataset.source;
+
+    if (!click) {
+        return
+    }
+
+    openModalWindow(chosenImg)
+
+    onEscKey(chosenImg);
+}
+
+function openModalWindow(picture){
+    const instance = basicLightbox.create(`
+        <img src=${picture}>
+    `)
+    instance.show()
+    
+    // onShow: (instance) => {}
+}
+
 
 console.log(galleryItems);
