cb4ec34dcdb886c3ca4a95e9912ce8c227f826e1
Maksym <lucky13375@gmail.com>
2022-09-09 11:47:10 +0200

    finished 2nd gallery

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup)
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+</a>`
+    }).join('')
+}
+
+createGalleryMarkup(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250
+});
+
 
 console.log(galleryItems);
